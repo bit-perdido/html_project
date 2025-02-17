9fd382d130ce59087353a0dcc54f9f1d92e9199d
bit-perdido <LAFidalgo@proton.me>
2025-02-17 23:20:20 +0100

    HMTL Node save

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    first = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(first)     
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+    
+    def test_eq_2(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_dif_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_dif_text(self):
+        node = TextNode("Thisa text node", TextType.BOLD, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_dif_type(self):
+        node = TextNode("Thisa text node", TextType.BOLD, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.NORMAL)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
